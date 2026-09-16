024f93c41a2b8b12ea36f551902a9a989d3c3d90
ducanhdao74-gif <ducanhdao74@gmail.com>
2026-09-16 13:57:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -427,7 +427,7 @@ if not str_app.session_state["app_started"]:
         """, unsafe_allow_html=True)
     str_app.markdown("<br>", unsafe_allow_html=True)
         
-        col_btn_center1, col_btn_center2, col_btn_center3 = str_app.columns([1, 1.5, 1])
+    col_btn_center1, col_btn_center2, col_btn_center3 = str_app.columns([1, 1.5, 1])
         with col_btn_center2:
             if str_app.button("✨ Bắt đầu vào ôn tập ngay", type="primary", use_container_width=True):
                 str_app.session_state["app_started"] = True

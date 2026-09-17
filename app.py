51d34d458e31adddd5d812b9e73c831a3e526516
ducanhdao74-gif <ducanhdao74@gmail.com>
2026-09-17 14:28:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -668,13 +668,12 @@ else:
                 
                 default_idx = None
                 current_saved_ans = str_app.session_state.get(ans_storage_key, None)
-                if current_saved_ans in options:
-                    default_idx = options.index(current_saved_ans)
-                    
-                else:
-                    saved_choice = str_app.session_state.get(ans_storage_key)
-                    str_app.markdown(f"<p style='color: #cbd5e1; font-size: 1.05rem;'>Đã chọn: <b>{saved_choice}</b></p>", unsafe_allow_html=True)
-        
+                saved_choice = current_saved_ans
+    if current_saved_ans in options:
+        default_idx = options.index(current_saved_ans)
+    else:
+        saved_choice = str_app.session_state.get(ans_storage_key)
+        str_app.markdown(f"<p style='color: #cbd5e1; font-size: 1.05rem;'>Đã chọn: <b>{saved_choice}</b></p>", unsafe_allow_html=True)
         if saved_choice is not None:
             is_correct = saved_choice.strip().upper().startswith(correct_letter)
         else:

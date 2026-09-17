1719a906848783a74824e2bbd84d1c6069824803
ducanhdao74-gif <ducanhdao74@gmail.com>
2026-09-17 14:09:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -711,8 +711,8 @@ else:
                         wrong_list = str_app.session_state["wrong_questions"]
                     if not wrong_list:
                         str_app.info("🎉 Hiện tại bạn chưa có câu trả lời sai nào được lưu lại.")
-            else:
-                str_app.write(f"Bạn đang có **{len(wrong_list)}** câu cần ôn tập lại.")
+                    else:
+                        str_app.write(f"Bạn đang có **{len(wrong_list)}** câu cần ôn tập lại.")
                 if "wrong_idx" not in str_app.session_state:
                     str_app.session_state["wrong_idx"] = 0
                     
